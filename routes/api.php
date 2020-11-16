4c22eec7aa6323a88ae5b6fffeeedcf8d4fe2e38
Melvin D. Protacio <melvin.protacio@zmtsys.com>
2020-11-16 12:47:31 +0800

    added routes middleware to cache results to 1 month

--- routes/api.php
+++ routes/api.php
@@ -11,24 +11,25 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware('cache.headers:public;max_age=2628000;etag')->group(function () {
+    Route::get('regions', 'RegionController@index')->name('region.index');
+    Route::get('regions/{region}', 'RegionController@show')->name('region.show');
 
-Route::get('regions', 'RegionController@index')->name('region.index');
-Route::get('regions/{region}', 'RegionController@show')->name('region.show');
+    Route::get('provinces', 'ProvinceController@index')->name('province.index');
+    Route::get('provinces/{province}', 'ProvinceController@show')->name('province.show');
 
-Route::get('provinces', 'ProvinceController@index')->name('province.index');
-Route::get('provinces/{province}', 'ProvinceController@show')->name('province.show');
+    Route::get('districts', 'DistrictController@index')->name('district.index');
+    Route::get('districts/{district}', 'DistrictController@show')->name('district.show');
 
-Route::get('districts', 'DistrictController@index')->name('district.index');
-Route::get('districts/{district}', 'DistrictController@show')->name('district.show');
+    Route::get('cities', 'CityController@index')->name('city.index');
+    Route::get('cities/{city}', 'CityController@show')->name('city.show');
 
-Route::get('cities', 'CityController@index')->name('city.index');
-Route::get('cities/{city}', 'CityController@show')->name('city.show');
+    Route::get('municipalities', 'MunicipalityController@index')->name('municipality.index');
+    Route::get('municipalities/{municipality}', 'MunicipalityController@show')->name('municipality.show');
 
-Route::get('municipalities', 'MunicipalityController@index')->name('municipality.index');
-Route::get('municipalities/{municipality}', 'MunicipalityController@show')->name('municipality.show');
+    Route::get('sub-municipalities', 'SubMunicipalityController@index')->name('sub-municipality.index');
+    Route::get('sub-municipalities/{subMunicipality}', 'SubMunicipalityController@show')->name('sub-municipality.show');
 
-Route::get('sub-municipalities', 'SubMunicipalityController@index')->name('sub-municipality.index');
-Route::get('sub-municipalities/{subMunicipality}', 'SubMunicipalityController@show')->name('sub-municipality.show');
-
-Route::get('barangays', 'BarangayController@index')->name('barangay.index');
-Route::get('barangays/{barangay}', 'BarangayController@show')->name('barangay.show');
+    Route::get('barangays', 'BarangayController@index')->name('barangay.index');
+    Route::get('barangays/{barangay}', 'BarangayController@show')->name('barangay.show');
+});
