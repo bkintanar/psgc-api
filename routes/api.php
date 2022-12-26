95987d5d6a5f7095231780b9cd17aad68d6a5779
Melvin D. Protacio <mdprotacio@outlook.com>
2022-12-27 07:42:45 +0800

    added cache headers as responses will never change anyway

--- routes/api.php
+++ routes/api.php
@@ -19,23 +19,25 @@ use App\Http\Controllers\ProvinceController;
 use App\Http\Controllers\MunicipalityController;
 use App\Http\Controllers\SubMunicipalityController;
 
-Route::get('regions', [RegionController::class, 'index'])->name('region.index');
-Route::get('regions/{region}', [RegionController::class, 'show'])->name('region.show');
+Route::middleware('cache.headers:public;max_age=3600;etag')->group(function(){
+    Route::get('regions', [RegionController::class, 'index'])->name('region.index');
+    Route::get('regions/{region}', [RegionController::class, 'show'])->name('region.show');
 
-Route::get('provinces', [ProvinceController::class, 'index'])->name('province.index');
-Route::get('provinces/{province}', [ProvinceController::class, 'show'])->name('province.show');
+    Route::get('provinces', [ProvinceController::class, 'index'])->name('province.index');
+    Route::get('provinces/{province}', [ProvinceController::class, 'show'])->name('province.show');
 
-Route::get('districts', [DistrictController::class, 'index'])->name('district.index');
-Route::get('districts/{district}', [DistrictController::class, 'show'])->name('district.show');
+    Route::get('districts', [DistrictController::class, 'index'])->name('district.index');
+    Route::get('districts/{district}', [DistrictController::class, 'show'])->name('district.show');
 
-Route::get('cities', [CityController::class, 'index'])->name('city.index');
-Route::get('cities/{city}', [CityController::class, 'show'])->name('city.show');
+    Route::get('cities', [CityController::class, 'index'])->name('city.index');
+    Route::get('cities/{city}', [CityController::class, 'show'])->name('city.show');
 
-Route::get('municipalities', [MunicipalityController::class, 'index'])->name('municipality.index');
-Route::get('municipalities/{municipality}', [MunicipalityController::class, 'show'])->name('municipality.show');
+    Route::get('municipalities', [MunicipalityController::class, 'index'])->name('municipality.index');
+    Route::get('municipalities/{municipality}', [MunicipalityController::class, 'show'])->name('municipality.show');
 
-Route::get('sub-municipalities', [SubMunicipalityController::class, 'index'])->name('sub-municipality.index');
-Route::get('sub-municipalities/{subMunicipality}', [SubMunicipalityController::class, 'show'])->name('sub-municipality.show');
+    Route::get('sub-municipalities', [SubMunicipalityController::class, 'index'])->name('sub-municipality.index');
+    Route::get('sub-municipalities/{subMunicipality}', [SubMunicipalityController::class, 'show'])->name('sub-municipality.show');
 
-Route::get('barangays', [BarangayController::class, 'index'])->name('barangay.index');
-Route::get('barangays/{barangay}', [BarangayController::class, 'show'])->name('barangay.show');
+    Route::get('barangays', [BarangayController::class, 'index'])->name('barangay.index');
+    Route::get('barangays/{barangay}', [BarangayController::class, 'show'])->name('barangay.show');
+});
